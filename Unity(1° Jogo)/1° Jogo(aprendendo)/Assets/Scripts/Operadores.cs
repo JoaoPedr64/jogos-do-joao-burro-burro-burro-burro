2104af208c24646c4beca8d17029fc4894262741
JoaoPedr64 <lidianlvs12@gmail.com>
2023-07-31 13:53:09 -0300

    nome

---
+++ Unity(1° Jogo)/1° Jogo(aprendendo)/Assets/Scripts/Operadores.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operadores : MonoBehaviour
+{
+    int number1 = 30, number2 = 20;
+    string resut;
+
+    void Start()
+    {
+       resut = number1>number2 ? "sim": "nao";
+       Debug.Log(resut);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
